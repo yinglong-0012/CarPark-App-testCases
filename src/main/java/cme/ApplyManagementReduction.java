614fa707b8cc12399a59dd1afd7c30b1ee00f9ca
yinglong-0012 <72497235+yinglong-0012@users.noreply.github.com>
2023-03-30 14:12:16 +0100

    update Management code
    the old version returns the minPayable function that does not work correctly, causing the test case to test less than minPayable should work in theory but the branch coverage is still 0 about Management. after updating the code of Management calculate the return value relevant code. the branch coverage be covered

--- src/main/java/cme/ApplyManagementReduction.java
+++ src/main/java/cme/ApplyManagementReduction.java
@@ -5,6 +5,6 @@ import java.math.BigDecimal;
 public class ApplyManagementReduction {
     public static BigDecimal calculate(BigDecimal totalCost) {
         BigDecimal minPayable = BigDecimal.valueOf(5.0);
-        return totalCost.max(minPayable);
+        return totalCost.compareTo(minPayable) <= 0 ? minPayable :totalCost;
     }
 }
